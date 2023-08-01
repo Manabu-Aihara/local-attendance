f2d925ff3d855ec84f360c8e251db7bc27bbe0ed
Manabu-Aihara <manabu@yaboiryo.co.jp>
2023-08-01 16:56:31 +0900

    feat #230801: 00:00:00表記を何とかしようと、ユーティリティ関数の残骸が

--- app/approval_util.py
+++ app/approval_util.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import TypeVar
 
 # class OutputApprovalDataConversion():
     
@@ -13,7 +14,7 @@ def convert_str_to_time(arg: datetime) -> datetime:
     if arg != '':
         return arg
     else:
-        conv_time_empty = arg.replace('%H:%M', '')      
+        conv_time_empty = arg.replace('%H:%M', '')
         return conv_time_empty
     
 def toggle_notification_type(table, arg: str | int) -> int | str:
@@ -27,3 +28,17 @@ def toggle_notification_type(table, arg: str | int) -> int | str:
     else:
         TypeError("intかstrのどちらかです")
 
+def search_uneffective_time_index(list_obj):
+    return [i for i, x in enumerate(list_obj) if x.START_TIME.strftime('%H:%M') == "00:00"]
+
+T = TypeVar('T')
+def replace_uneffective_index_time(list_obj: list[T]) -> list[str]:
+    index: list = [i for i, x in enumerate(list_obj) if x.START_TIME.strftime('%H:%M') == "00:00"]
+    for i in index:
+        list_obj[i].START_TIME = _replace(list_obj[i])
+        return list_obj[i].START_TIME.strftime('')
+
+def _replace(t_object: T) -> datetime:
+    zero_date_mod: str = t_object.START_TIME.strftime('%H:%M:%S').replace("00:00:00", "")
+    zero_date = datetime.strptime(zero_date_mod, "")
+    return zero_date
--- app/routes_approvals.py
+++ app/routes_approvals.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, time
 
 from flask import render_template, redirect, request
 from flask_login import current_user
@@ -10,10 +10,10 @@ from app.common_func import GetPullDownList
 from app.models import StaffLoggin, Todokede
 from app.models_aprv import NotificationList
 from app.approval_util import (convert_str_to_date, convert_str_to_time,
-                               toggle_notification_type)
+                               toggle_notification_type, replace_uneffective_index_time)
 
 """
-    戻り値に代入される変数は、必ずstf_login！！
+    戻り値に代入される変数名は、必ずstf_login！！
     """
 def appare_global_staff() -> StaffLoggin:
     current_staff = StaffLoggin.query.get(current_user.STAFFID)
@@ -23,10 +23,39 @@ def appare_global_staff() -> StaffLoggin:
 @login_required
 def get_approval_list(STAFFID):
     notification_list = NotificationList.query.filter(NotificationList.STAFFID == STAFFID).all()
+
+    # for nlst in notification_list:
+    #     nlst.START_TIME.strftime('%H:%M').replace("00:00:00", "")
+    #     nlst.END_TIME.strftime('%H:%M').replace("00:00:00", "")
+    # out_index = search_uneffective_time_index(notification_list)
+    # for oi in out_index:
+    #     notification_list[oi].START_TIME.strftime('%H:%M:%S').replace("00:00:00", "")
+    #     notification_list[oi].END_TIME.strftime('%H:%M:%S').replace("00:00:00", "")
+    # type_list_time = replace_uneffective_index_time(notification_list)
+    # for lt in type_list_time:
+    #     return lt
+    
+    # return notification_list[3].START_TIME.strftime('%H:%M:%S')
     return render_template('attendance/notification_list.html', 
                            nlst=notification_list,
                            stf_login=appare_global_staff()
                            )
+
+@app.route('/confirm/<STAFFID>/<id>', methods=['GET'])
+@login_required
+def get_individual_approval(STAFFID, id: int):
+    notification_row = NotificationList.query.get(id)
+
+    table_columns = []
+    # https://teratail.com/questions/214028
+    table_columns.append(NotificationList.__table__.c.keys()[2:])
+
+    date_list = []
+    date_list.append(NotificationList.query.filter(notification_row.N_CODE==table_columns[0])).first()
+    
+    # return render_template('attendance/approval_confirm.html',
+    #                        one_data=notification_row,
+    #                        stf_login=appare_global_staff())
 
 def get_notification_list():
     todokede_list = GetPullDownList(Todokede, Todokede.CODE, Todokede.NAME,
@@ -57,8 +86,8 @@ def retrieve_form_data(list_data: list[str]) -> list:
 def post_approval():
     approval_list = ['start-day', 'end-day', 'start-time', 'end-time', 'remark']
     form_list_data = retrieve_form_data(approval_list)
+    # 数値(CODE)を文字列(NAME)に入れ替え
     form_content: str = toggle_notification_type(Todokede, int(request.form.get('content')))
-    # return form_content
     form_list_data.insert(0, form_content)
 
     return render_template('attendance/approval_confirm.html', 
@@ -70,11 +99,12 @@ def post_approval():
 def append_approval():
     # out_apprpval_valid = OutputApprovalDataConversion()
 
+    # こちらは数値(CODE)に変換
     form_content: int = toggle_notification_type(Todokede, request.form.get('content'))
     form_start_day: datetime = convert_str_to_date(request.form.get('start-day'))
-    form_end_day: datetime | str = convert_str_to_date(request.form.get('end-day'))
-    form_start_time: datetime | str = convert_str_to_time(request.form.get('start-time'))
-    form_end_time: datetime | str = convert_str_to_time(request.form.get('end-time'))
+    form_end_day: datetime = convert_str_to_date(request.form.get('end-day'))
+    form_start_time: datetime = convert_str_to_time(request.form.get('start-time'))
+    form_end_time: datetime = convert_str_to_time(request.form.get('end-time'))
     form_remark = request.form.get('remark')
 
     one_notification = NotificationList(
