3661827e252aee9caa59c446fd1d981ae405fd8d
Manabu-Aihara <manabu@yaboiryo.co.jp>
2023-10-30 13:01:29 +0900

    chore #こっちはまだJSON無理

--- app/routes_sub.py
+++ app/routes_sub.py
@@ -5,11 +5,37 @@ from flask.helpers import url_for
 
 from app import app, db
 
+from marshmallow import Schema, fields, post_load
+
+class ModelDummy():
+  def __init__(self, comment) -> None:
+    self.comment = comment
+
+class DummySchema(Schema):
+  comment = fields.Str(require=True)
+
+  @post_load
+  def build(self, data, **kwargs):
+    return ModelDummy(**data)
+
 @app.route('/dummy/<target_id>', methods=['GET', 'POST'])
 @login_required
-def action_sub(target_id):
-  if request.method == 'GET':
+def appear_sub(target_id):
+  # if request.method == 'GET':
     return render_template('admin/dummy.html',
                            t_num=target_id, stf_login=current_user)
 
-  # elif request.method == 'POST':
+  # 10月27日追加
+  # dummyデータ受取
+  # response = {}
+  # if request.method == 'POST':
+@app.route('/dummy/display', methods=['POST'])
+@login_required
+def appear_json():
+  schema = DummySchema()
+  response: dict = request.json['comment']
+  resp_data = ModelDummy(comment=response)
+  json_data = schema.dump(resp_data)
+
+  # return json_data
+  return render_template('admin/edit_data_user.html', res=json_data, stf_login=current_user)
--- app/static/js/SubAction.js
+++ app/static/js/SubAction.js
@@ -3,13 +3,14 @@ const targetNumber = document.getElementById('target-number');
 // edit_data_user
 let commentArea = document.querySelector('.dummy-comment');
 // console.log(commentArea);
+const url = `/admin/edit_data_user/${targetNumber.textContent}/1`
 
 reflectButton.addEventListener('click', (e) => {
   e.preventDefault();
   const subValue = document.getElementsByClassName('test-value')[0];
   console.log(subValue.value);
   // commentArea.textContent = subValue.value;
-  fetch(`/admin/edit_data_user/${targetNumber.textContent}/1`, {
+  fetch(url, {
     method: 'POST',
     headers: {
       'Access-Control-Allow-Origin': '*',
@@ -22,7 +23,7 @@ reflectButton.addEventListener('click', (e) => {
       'comment': subValue.value
     })
   })
-  // .then(res => res.json())
-  .then(res => console.log(res.json))
+  .then(res => res.json())
+  .then(json => console.log(`投げた：${json}`));
   // .catch(err => console.log(err.response))
 });
