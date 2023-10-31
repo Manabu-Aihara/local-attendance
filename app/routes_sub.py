2fb2a4f65b70508d2f4055b6073d5ae21c127645
Manabu-Aihara <manabu@yaboiryo.co.jp>
2023-10-31 14:38:48 +0900

    chore #231031: サブウィンドウ試作品完成

--- app/routes_sub.py
+++ app/routes_sub.py
@@ -1,41 +1,27 @@
 from flask import render_template, redirect, request
 from flask_login import current_user
 from flask_login.utils import login_required
-from flask.helpers import url_for
 
 from app import app, db
+from app.dummy_model_todo import TodoOrm
 
-from marshmallow import Schema, fields, post_load
 
-class ModelDummy():
-  def __init__(self, comment) -> None:
-    self.comment = comment
-
-class DummySchema(Schema):
-  comment = fields.Str(require=True)
-
-  @post_load
-  def build(self, data, **kwargs):
-    return ModelDummy(**data)
-
-@app.route('/dummy/<target_id>', methods=['GET', 'POST'])
+@app.route("/dummy-form/<target_id>", methods=["GET"])
 @login_required
 def appear_sub(target_id):
-  # if request.method == 'GET':
-    return render_template('admin/dummy.html',
-                           t_num=target_id, stf_login=current_user)
+    recent_todo = TodoOrm.query.order_by(TodoOrm.id.desc()).first()
+    return render_template(
+        "admin/dummy.html", t_num=target_id, recent=recent_todo, stf_login=current_user
+    )
 
-  # 10月27日追加
-  # dummyデータ受取
-  # response = {}
-  # if request.method == 'POST':
-@app.route('/dummy/display', methods=['POST'])
+
+@app.route("/todo/add", methods=["POST"])
 @login_required
-def appear_json():
-  schema = DummySchema()
-  response: dict = request.json['comment']
-  resp_data = ModelDummy(comment=response)
-  json_data = schema.dump(resp_data)
+def append_todo():
+    summary = request.json["summary"]
+    owner = request.json["owner"]
+    one_todo = TodoOrm(summary=summary, owner=owner)
+    db.session.add(one_todo)
+    db.session.commit()
 
-  # return json_data
-  return render_template('admin/edit_data_user.html', res=json_data, stf_login=current_user)
+    return redirect("/dummy-form")
--- app/static/js/SubAction.js
+++ app/static/js/SubAction.js
@@ -1,16 +1,19 @@
 const reflectButton = document.getElementById('reflect');
 const targetNumber = document.getElementById('target-number');
-// edit_data_user
-let commentArea = document.querySelector('.dummy-comment');
-// console.log(commentArea);
-const url = `/admin/edit_data_user/${targetNumber.textContent}/1`
+// dummy.html
+const summaryElm = document.getElementsByClassName('summary-form')[0];
+const ownerElm = document.getElementsByClassName('owner-form')[0];
+// const url = `/admin/edit_data_user/${targetNumber.textContent}/1`
 
 reflectButton.addEventListener('click', (e) => {
   e.preventDefault();
-  const subValue = document.getElementsByClassName('test-value')[0];
-  console.log(subValue.value);
-  // commentArea.textContent = subValue.value;
-  fetch(url, {
+  // To edit_data_user.html
+  let outputSummary = window.opener.document.getElementsByClassName('dummy-summary')[0];
+  let outputOwner = window.opener.document.getElementsByClassName('dummy-owner')[0];
+  outputSummary.value = summaryElm.value;
+  outputOwner.value = ownerElm.value;
+
+  fetch('http://127.0.0.1:8000/todo/add', {
     method: 'POST',
     headers: {
       'Access-Control-Allow-Origin': '*',
@@ -18,12 +21,16 @@ reflectButton.addEventListener('click', (e) => {
       // Accept: 'application/json',
       'Content-Type': 'application/json'
     },
-    // body: subValue.value
     body: JSON.stringify({
-      'comment': subValue.value
+      summary: summaryElm.value,
+      owner: ownerElm.value
     })
   })
-  .then(res => res.json())
-  .then(json => console.log(`投げた：${json}`));
-  // .catch(err => console.log(err.response))
+    .then(res => res.json())
+    .then(json => console.log(`投げた：${json}`))
+    // .catch(err => console.log(err.response))
+    .finally(() => {
+      summaryElm.value = "";
+      ownerElm.value = "";
+    });
 });
